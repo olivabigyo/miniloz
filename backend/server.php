a80cb8ac5139afaa379be058d8ff77c04df36656
Kata <35190838+olivabigyo@users.noreply.github.com>
2021-03-13 20:58:46 +0100

    comments to this stage

--- backend/server.php
+++ backend/server.php
@@ -1,15 +1,8 @@
 <?php
 header('Content-Type: application/json');
-// header('Content-Type: text/plain');
+// header('Content-Type: text/plain');  // at developing and testing with print_r
 
-function exitWithError($errorMessage)
-{
-    echo json_encode(array('ok' => false, 'error' => $errorMessage));
-    exit;
-}
-
-// $request = json_decode(file_get_contents('php://input'));
-
+// We use this templates to define the room tiles from the random generated wall connections
 $templates = array(
     '0000' => array('image' => 'null.svg', 'connections' => '0000'),
     '0001' => array('image' => 'one.svg', 'connections' => '0100'),
@@ -29,16 +22,16 @@ $templates = array(
     '1111' => array('image' => 'four.svg', 'connections' => '1111'),
 );
 
-
-
 function createRoom($w, $h)
 {
+    // First we decide which walls are connected in the room
+    // We use an array for vertical walls and an array for the horizontal walls
     $vWalls = array();
     // Make a h x (w+1) array filled with zeros
     for ($r = 0; $r < $h; $r++) {
         $vWalls[] = array_fill(0, $w + 1, 0);
     }
-    // Randomize the values
+    // Randomize the values only of the inner walls
     for ($r = 0; $r < $h; $r++) {
         for ($c = 1; $c < $w; $c++) {
             $vWalls[$r][$c] = rand(0, 1);
@@ -49,25 +42,33 @@ function createRoom($w, $h)
     for ($r = 0; $r < $h + 1; $r++) {
         $hWalls[] = array_fill(0, $w, 0);
     }
+    // Randomize the values only of the inner walls
     for ($r = 1; $r < $h; $r++) {
         for ($c = 0; $c < $w; $c++) {
             $hWalls[$r][$c] = rand(0, 1);
         }
     }
+    // Create tiles
     $tiles = array();
     for ($r = 0; $r < $h; $r++) {
         $row = array();
         for ($c = 0; $c < $w; $c++) {
+            // The tile connections-string comes from the vertical and horizontal collection of walls
             $t = '';
             $t .= $hWalls[$r][$c];
             $t .= $vWalls[$r][$c + 1];
             $t .= $hWalls[$r + 1][$c];
             $t .= $vWalls[$r][$c];
+            // peek from template the default image and default connection using the tile connections-string
             global $templates;
             $image = $templates[$t]['image'];
             $connections = $templates[$t]['connections'];
-            $row[] = array('image' => $image, 'connections' => $connections, 'rotation' => rand(0, 3));
+            // define random rotation to the tiles
+            $rotation = rand(0,3);
+            // fill the tiles in the row array
+            $row[] = array('image' => $image, 'connections' => $connections, 'rotation' => $rotation);
         }
+        // fill the rows in the tiles array
         $tiles[] = $row;
     }
     // print_r($tiles);
@@ -76,6 +77,8 @@ function createRoom($w, $h)
 
 $w = 4;
 $h = 4;
+
+// Our server response
 echo json_encode(array(
     'ok' => true,
     'room' => array('id' => 1, 'name' => 'main', 'w' => $w, 'h' => $h, 'tiles' => createRoom($w, $h))
--- frontend/code.js
+++ frontend/code.js
@@ -1,33 +1,69 @@
-const canvas = document.getElementById('canvas')
+const playground = document.getElementById('canvas')
 const nullTile = { image: '', connections: '0000', rotation: 0 };
-let room = {
-    id: 1, name: 'main', w: 4, h: 4, tiles:
-        [[
-            { image: 'two.svg', connections: '1001', rotation: 0 },
-            { image: 'two-straight.svg', connections: '0101', rotation: 0 },
-            { image: 'three.svg', connections: '1011', rotation: 0 },
-            { image: 'one.svg', connections: '0100', rotation: 0 },
-        ],
-        [
-            { image: 'three.svg', connections: '1011', rotation: 0 },
-            { image: 'two-straight.svg', connections: '0101', rotation: 0 },
-            { image: 'four.svg', connections: '1111', rotation: 0 },
-            { image: 'two.svg', connections: '1001', rotation: 0 },
-        ],
-        [
-            { image: 'two.svg', connections: '1001', rotation: 0 },
-            { image: 'three.svg', connections: '1011', rotation: 0 },
-            { image: 'three.svg', connections: '1011', rotation: 0 },
-            { image: 'three.svg', connections: '1011', rotation: 0 },
-        ],
-        [
-            { image: 'null.svg', connections: '0000', rotation: 0 },
-            { image: 'one.svg', connections: '0100', rotation: 0 },
-            { image: 'null.svg', connections: '0000', rotation: 0 },
-            { image: 'one.svg', connections: '0100', rotation: 0 },
-        ]]
-};
+let room = {};
+const apiEndpoint = 'http://localhost/miniloz/backend/server.php';
 
+async function getRoom() {
+    // console.log('Requesting new room from server...');
+
+    try {
+        const request = await fetch(apiEndpoint);
+
+        if (request.status != 200) {
+            console.log('Fetch bad status: ' + request.status);
+            return;
+        }
+
+        const data = await request.json();
+
+        if (!data.ok) {
+            console.log('Server returned error: ' + data.error);
+            return;
+        }
+
+        console.log('Successful request.', data);
+
+        room = data.room;
+        init();
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+getRoom();
+
+// This is a 4x4 room for the first prototype before writing getRoom and without the serverside createRoom:
+// The directions in the connections-string are: up, right, down, left
+// let room = {
+//     id: 1, name: 'main', w: 4, h: 4, tiles:
+//         [[
+//             { image: 'two.svg', connections: '1001', rotation: 0 },
+//             { image: 'two-straight.svg', connections: '0101', rotation: 0 },
+//             { image: 'three.svg', connections: '1011', rotation: 0 },
+//             { image: 'one.svg', connections: '0100', rotation: 0 },
+//         ],
+//         [
+//             { image: 'three.svg', connections: '1011', rotation: 0 },
+//             { image: 'two-straight.svg', connections: '0101', rotation: 0 },
+//             { image: 'four.svg', connections: '1111', rotation: 0 },
+//             { image: 'two.svg', connections: '1001', rotation: 0 },
+//         ],
+//         [
+//             { image: 'two.svg', connections: '1001', rotation: 0 },
+//             { image: 'three.svg', connections: '1011', rotation: 0 },
+//             { image: 'three.svg', connections: '1011', rotation: 0 },
+//             { image: 'three.svg', connections: '1011', rotation: 0 },
+//         ],
+//         [
+//             { image: 'null.svg', connections: '0000', rotation: 0 },
+//             { image: 'one.svg', connections: '0100', rotation: 0 },
+//             { image: 'null.svg', connections: '0000', rotation: 0 },
+//             { image: 'one.svg', connections: '0100', rotation: 0 },
+//         ]]
+// };
+
+// define the tile from position
 function getTile(r, c) {
     return room.tiles[r][c];
 }
@@ -37,14 +73,14 @@ function init() {
     for (const row of room.tiles) {
         let c = 0;
         for (const tile of row) {
-            // console.log('just do it!');
+            // console.log('just counting...');
             const elem = document.createElement('div');
             elem.classList.add('tile');
             tile.elem = elem;
             tile.deg = tile.rotation * 90;
             elem.style.transform = `rotate(${tile.deg}deg)`;
             elem.style.backgroundImage = `url(img/${tile.image})`;
-            canvas.appendChild(elem);
+            playground.appendChild(elem);
             tile.r = r;
             tile.c = c;
             elem.addEventListener('click', () => {
@@ -55,6 +91,7 @@ function init() {
         }
         r++;
     }
+    // Mark the connected tiles
     for (const row of room.tiles) {
         for (const tile of row) {
             isConnected(tile);
@@ -62,16 +99,19 @@ function init() {
     }
 }
 
-// init();
-
 function clicked(tile) {
     // console.log('row: ', tile.r, 'column: ', tile.c);
+
+    // increase values
     tile.rotation = (tile.rotation + 1) % 4;
     tile.deg += 90;
+    // rotate
     tile.elem.style.transform = `rotate(${tile.deg}deg)`;
+    // mark new connections
     checkConnections(tile);
 }
 
+// Mark the clicked tile and neighbours if connected
 function checkConnections(tile) {
     isConnected(tile);
     isConnected(topNeighbour(tile));
@@ -80,13 +120,7 @@ function checkConnections(tile) {
     isConnected(leftNeighbour(tile));
 }
 
-// directions:
-const UP = 0;
-const RIGHT = 1;
-const DOWN = 2;
-const LEFT = 3;
-
-// Neigbouring positions
+// Define neigbouring tiles
 function topNeighbour(t) {
     if (t.r > 0) {
         return getTile(t.r - 1, t.c);
@@ -116,59 +150,31 @@ function leftNeighbour(t) {
     }
 };
 
+// Directions
+const UP = 0;
+const RIGHT = 1;
+const DOWN = 2;
+const LEFT = 3;
+
+// Mark a tile if it is connected in all side with neigbours
 function isConnected(tile) {
     if (tile == nullTile) return;
+    // using the rotation and connections-string of the tile we know
+    // which side of the tile has a pipe-part to connect (1) and which hasn't (0)
     if (
+        // comparing the right side of the tile with the left side of the neighbour tile
         (tile.connections[(4 + RIGHT - tile.rotation) % 4] == rightNeighbour(tile).connections[(4 + LEFT - rightNeighbour(tile).rotation) % 4])
+        // comparing the bottom side of the tile with the top side of the neighbour tile
         && (tile.connections[(4 + DOWN - tile.rotation) % 4] == bottomNeighbour(tile).connections[(4 + UP - bottomNeighbour(tile).rotation) % 4])
+        // comparing the left side of the tile with the right side of the neighbour tile
         && (tile.connections[(4 + LEFT - tile.rotation) % 4] == leftNeighbour(tile).connections[(4 + RIGHT - leftNeighbour(tile).rotation) % 4])
+        // comparing the top side of the tile with the bottom side of the neighbour tile
         && (tile.connections[(4 + UP - tile.rotation) % 4] == topNeighbour(tile).connections[(4 + DOWN - topNeighbour(tile).rotation) % 4])
     ) {
+        // tile is connected in all sides
         tile.elem.style.opacity = 1;
     } else {
+        // at least on side doesn't fit with the neigbour tile
         tile.elem.style.opacity = 0.7;
     }
 }
-
-
-
-
-//
-//
-// ////////////////////////////////////////////////////////////////////////////
-
-const apiEndpoint = 'http://localhost/miniloz/backend/server.php';
-
-async function getRoom() {
-    // log(`Requesting ${operation} from server...`);
-
-    try {
-        const request = await fetch(apiEndpoint, {
-            // method: 'POST',
-            // headers: {
-                // 'Content-Type': 'application/json'
-            // },
-            // body: JSON.stringify({ operation, payload })
-        });
-        if (request.status != 200) {
-            console.log('Fetch bad status: ' + request.status);
-            return;
-        }
-
-        const data = await request.json();
-        if (!data.ok) {
-            console.log('Server returned error: ' + data.error);
-            return;
-        }
-
-        console.log('Successful request.', data);
-        room = data.room;
-        init();
-    } catch (err) {
-        // Error handling
-        console.log(err);
-        // logError('Exeption: ' + err);
-    }
-}
-
-getRoom();
