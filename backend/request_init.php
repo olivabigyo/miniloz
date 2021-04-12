86698fbd2b3d0c5d4eda5998836ebfa0ad26c36e
Kata <35190838+olivabigyo@users.noreply.github.com>
2021-04-12 17:16:15 +0200

    Refactor initial request processing

---
+++ backend/request_init.php
@@ -0,0 +1,49 @@
+<?php
+// we handle requests from anyone
+// TODO: This doesn't work with credentials: include
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
+
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === 'OPTIONS') {
+    // We have set all the necessary CORS headers, just return.
+    exit;
+}
+
+function exitWithError($errorMessage)
+{
+    echo json_encode(array('ok' => false, 'error' => $errorMessage));
+    exit;
+}
+
+if ($method !== 'POST') {
+    exitWithError('Only POST requests allowed');
+}
+
+$request = json_decode(file_get_contents('php://input'));
+$action = $request->action;
+$payload = $request->payload;
+
+$validActions = array(
+    // user handling
+    'login',
+    'createUser',
+    // chat
+    'getMessages',
+    'addMesage',
+    // game
+    'getNewRoom',
+);
+
+if (!in_array($action, $validActions)) {
+    exitWithError('Unrecognized action');
+}
+
+session_start(array(
+    // use different cookie from the default PHPSESSID
+    'name' => 'LOOPSSESSID',
+    // JS should not access the session cookie
+    'cookie_httponly' => true,
+));
--- backend/server.php
+++ backend/server.php
@@ -1,48 +1,24 @@
 <?php
+require_once 'request_init.php';
+
 require_once 'load.php';
 require_once 'class/Game.class.php';
 
-// we handle requests from anyone
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Content-Type: application/json');
+if ($action === 'getNewRoom') {
+    $w = $payload->size;
+    $h = $w;
+    $density = $payload->density;
 
-$method = $_SERVER['REQUEST_METHOD'];
-if ($method === 'OPTIONS') {
-    // We have set all the necessary CORS headers, just return.
-    exit;
-}
+    $game = Game::generate($w, $h, $density);
 
-if ($method !== 'POST') {
-    echo json_encode(array('ok' => false, 'error' => 'Only POST requests allowed'));
-    exit;
-}
-
-$request = json_decode(file_get_contents('php://input'));
-$action = $request->action;
-$payload = $request->payload;
-
-if ($action !== 'getNewRoom') {
+    // Our server response
     echo json_encode(array(
-        'error' => 'Unrecognized action'
+        'ok' => true,
+        // 'error' => 'valami',
+        'room' => array(
+            'id' => 1,
+            'name' => 'main',
+            'game' => $game
+        )
     ));
-    exit;
 }
-
-$w = $payload->size;
-$h = $w;
-$density = $payload->density;
-
-$game = Game::generate($w, $h, $density);
-
-// Our server response
-echo json_encode(array(
-    'ok' => true,
-    // 'error' => 'valami',
-    'room' => array(
-        'id' => 1,
-        'name' => 'main',
-        'game' => $game
-    )
-));
