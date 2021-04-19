44b1dc2cb7d4602a8c25836eabbc2b5ef1934d44
Kata <35190838+olivabigyo@users.noreply.github.com>
2021-04-19 12:27:32 +0200

    Refactor request handling

--- backend/server.php
+++ backend/server.php
@@ -6,84 +6,54 @@ require_once 'class/Game.class.php';
 require_once 'class/User.class.php';
 require_once 'class/Chat.class.php';
 
-// User stuff
-if ($action === 'createUser') {
-    try {
-        echo json_encode(array(
-            'ok' => true,
-            'result' => User::createUser($payload)
-        ));
-    } catch (Exception $e) {
-        exitWithError($e->getMessage());
+function handleRequest($action, $payload)
+{
+    // User stuff
+    if ($action === 'createUser') {
+        return ['result' => User::createUser($payload)];
     }
-}
 
-if ($action === 'login') {
-    try {
-        echo json_encode(array(
-            'ok' => true,
-            'user' => User::login($payload)
-        ));
-    } catch (Exception $e) {
-        exitWithError($e->getMessage());
+    if ($action === 'login') {
+        return ['user' => User::login($payload)];
     }
-}
 
-if ($action === 'logout') {
-    try {
+    if ($action === 'logout') {
         User::logout();
-        echo json_encode(array(
-            'ok' => true
-        ));
-    } catch (Exception $e) {
-        exitWithError($e->getMessage());
+        return [];
     }
-}
 
-// Game stuff
-if ($action === 'getNewRoom') {
-    try {
-        $user = User::requireLogin();
+    // Things below here require a valid login
+    $user = User::requireLogin();
 
+    // Game stuff
+    if ($action === 'getNewRoom') {
         $w = $payload->size;
         $h = $w;
         $density = $payload->density;
 
         $game = Game::generate($w, $h, $density);
 
-        // Our server response
-        echo json_encode(array(
-            'ok' => true,
-            'room' => array(
-                'id' => 1,
-                'name' => 'main',
-                'game' => $game
-            )
-        ));
-    } catch (Exception $e) {
-        exitWithError($e->getMessage());
+        return ['room' => [
+            'id' => 1,
+            'name' => 'main',
+            'game' => $game
+        ]];
+    }
+
+    // Chat stuff
+    if ($action === 'getMessages') {
+        return ['messages' => Chat::getMessages()];
+    }
+
+    if ($action === 'addMessage') {
+        return ['id' => Chat::addMessage($payload)];
     }
 }
 
-// Chat stuff
-if ($action === 'getMessages') {
-    try {
-        echo json_encode([
-            'ok' => true,
-            'messages' => Chat::getMessages()
-        ]);
-    } catch (Exception $e) {
-        exitWithError($e->getMessage());
-    }
-}
-
-if ($action === 'addMessage') {
-    try {
-        echo json_encode([
-            'ok' => true,
-            'id' => Chat::addMessage($payload)
-        ]);
-    } catch (Exception $e) {
-        exitWithError($e->getMessage());
-    }
+try {
+    $response = handleRequest($action, $payload);
+    $response['ok'] = true;
+    echo json_encode($response);
+} catch (Exception $e) {
+    exitWithError($e->getMessage());
 }
